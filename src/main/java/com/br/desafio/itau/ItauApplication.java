dfb3d3b7a2f911475d291a3773ae44ba23f3884f
wevkleyton <kleyton.wev@gmail.com>
2025-05-06 17:40:48 -0300

    Criacao do projeto desafio Itau

---
+++ src/main/java/com/br/desafio/itau/ItauApplication.java
@@ -0,0 +1,13 @@
+package com.br.desafio.itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauApplication.class, args);
+	}
+
+}
